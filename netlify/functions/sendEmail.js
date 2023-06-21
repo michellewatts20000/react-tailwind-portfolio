fd7014db84998b2a2cbc686bfc21544914f46932
Your NameMichelle <watts.e.michelle@gmail.com>
2023-06-21 14:01:26 +1000

    update return from netlify function

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,47 +1,43 @@
 const nodemailer = require('nodemailer');
-const { promisify } = require('util');
 
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
     user: 'watts.e.michelle@gmail.com',
     pass: 'xgcejfzenjyavvof'
-  }
+  },
 });
 
 exports.handler = async function (event) {
-  const { name, email, message } = JSON.parse(event.body);
+  const { name, email, message } = event.body;
+  console.log(`Sending email to ${name}`);
 
-  try {
-    console.log(`Sending email to ${name}`);
-  
-    const sendMailAsync = promisify(transporter.sendMail).bind(transporter);
-  
-    const mailOptions = {
-      from: 'watts.e.michelle@gmail.com',
-      to: 'watts.e.michelle@gmail.com',
-      subject: 'New Contact Form Submission',
-      text: `
-        Name: ${name}
-        Email: ${email}
-        Message: ${message}
-      `
-    };
-  
-    await sendMailAsync(mailOptions);
-  
-    console.log(`Email sent to ${name}`);
-  
-    return {
-      statusCode: 200,
-      body: 'Email sent successfully'
-    };
-  } catch (error) {
-    console.error(error);
-  
-    return {
-      statusCode: 500,
-      body: 'An error occurred while sending the email'
-    };
-  }
+  // Define the email options
+  const mailOptions = {
+    from: 'watts.e.michelle@gmail.com',
+    to: 'watts.e.michelle@gmail.com',
+    subject: 'New Contact Form Submission',
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Message: ${message}
+    `,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      return {
+        statusCode: 200,
+        body: 'Email sent successfully'
+      }
+    } else {
+      return {
+        statusCode: 500,
+        body: 'An error occurred while sending the email'
+      }
+    }
+  });
+
+  console.log(`Email sent to ${name}`);
 };

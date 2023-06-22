69c8985971c913669dbed946f313ca92d8c2a47e
Your NameMichelle <watts.e.michelle@gmail.com>
2023-06-22 11:05:41 +1000

    scroll to top button

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,39 +1,43 @@
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes, faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMobileView, setIsMobileView] = useState(false);
+  const [showScrollButton, setShowScrollButton] = useState(false); // New state
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   const handleWindowResize = () => {
-    setIsMobileView(window.innerWidth < 1024)
+    setIsMobileView(window.innerWidth < 1024);
     if (window.innerWidth >= 1024) {
       setIsMenuOpen(false);
     }
   };
 
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0);
+    setShowScrollButton(window.scrollY > 0); // Show scroll button when scrolled
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
-
     window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', handleWindowResize); // Add resize event listener
-
-    // Initial check for mobile view
+    window.addEventListener('resize', handleWindowResize);
     handleWindowResize();
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('resize', handleWindowResize); // Clean up resize event listener
+      window.removeEventListener('resize', handleWindowResize);
     };
   }, []);
 
@@ -44,7 +48,6 @@ const Navigation = () => {
   };
 
   const navItems = [
-    { text: 'Home', href: '#hero', icon: null },
     { text: 'Skills', href: '#skills', icon: null },
     { text: 'Portfolio', href: '#portfolio', icon: null },
     { text: 'Contact', href: '#contact', icon: null },
@@ -78,15 +81,13 @@ const Navigation = () => {
           </div>
         </div>
         <ul
-          className={`${
-            isMenuOpen ? 'block' : 'hidden'
-          } lg:flex lg:space-x-4 items-center ${
-            isMobileView ? 'flex flex-col mt-20 text-2xl h-screen bg-black bg-opacity-50 z-index:5' : 'justify-end'
-          } lg:space-x-8 mt-8 lg:mt-0`}
+          className={`${isMenuOpen ? 'block' : 'hidden'
+            } lg:flex lg:space-x-4 items-center ${isMobileView ? 'flex flex-col mt-20 text-2xl h-screen bg-black bg-opacity-50 z-index:5' : 'justify-end'
+            } lg:space-x-8 mt-8 lg:mt-0`}
           style={{ zIndex: 30 }}
         >
           {navItems.map((item, index) => (
-            <li key={index} className='mb-3'>
+            <li key={index} className="mb-3">
               {item.href.startsWith('#') ? (
                 <a href={item.href} className="text-white text-md hover:text-primary-500 hover:underline" onClick={handleNavigationClick}>
                   {item.text}
@@ -105,6 +106,16 @@ const Navigation = () => {
             </li>
           ))}
         </ul>
+        {showScrollButton && ( // Render scroll button conditionally
+          <button
+            className="fixed right-10 bottom-6 bg-white rounded-full pt-4 px-4 pb-3 shadow-md focus:outline-none opacity-75 hover:opacity-100 transition-opacity duration-300"
+            onClick={scrollToTop}
+            style={{ zIndex: 40 }}
+          >
+            <FontAwesomeIcon icon={faArrowCircleUp} className="text-secondary-500 text-2xl" />
+          </button>
+
+        )}
       </div>
     </nav>
   );

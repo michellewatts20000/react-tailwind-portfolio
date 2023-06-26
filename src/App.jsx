4a693529c5dd2ee63571513bb6c80a35fb351523
Your NameMichelle <watts.e.michelle@gmail.com>
2023-06-27 09:14:37 +1000

    GA analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ import Contact from './Contact';
 import ShapeDivider from './ShapeDivider';
 import Footer from './Footer';
 import Skills from './Skills';
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
+ReactGA.initialize('G-TGYEXF5GNH');
+
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location])
+  
   return (
     <>
 

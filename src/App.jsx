47bdb4506365aac7bb3f39436e3d98923492757c
Your NameMichelle <watts.e.michelle@gmail.com>
2023-06-27 12:15:40 +1000

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import HeroSection from './Hero';
 import Navigation from './Navigation';
 import Portfolio from './Portfolio';
@@ -5,41 +7,26 @@ import Contact from './Contact';
 import ShapeDivider from './ShapeDivider';
 import Footer from './Footer';
 import Skills from './Skills';
-import ReactGA from 'react-ga';
-import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-
-
-
-
 
 const App = () => {
-  const location = useLocation();
+  const tracking_ID = 'G-Z5SWHFG9S1';
 
   useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location])
+    ReactGA.initialize(tracking_ID);
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+  }, [tracking_ID]);
 
   return (
     <>
-
       <Navigation />
-
       <HeroSection />
-
       <Skills />
-
       <Portfolio />
-
       <ShapeDivider foregroundColor="#DFDCE3" backgroundColor="#F7B733" />
-
       <Contact />
-
       <Footer />
-
     </>
   );
 };
 
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ReactGA from 'react-ga';
-
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.css';
 import "@fontsource/roboto"
 import "@fontsource/lora"
 
-ReactGA.initialize('G-TGYEXF5GNH');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
